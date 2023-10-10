90d259a1157306f1612a56d3afb9705c63360ee0
Alberto Abella <alberto.abella@fiware.org>
2023-10-10 13:41:35 +0200

    test of SQL export

---
+++ TerminalFacility/schema.sql
@@ -0,0 +1,3 @@
+/* (Beta) Export of data model TerminalFacility of the subject dataModel.ACRIS for a PostgreSQL database. Pending translation of enumerations and multityped attributes */
+CREATE TYPE TerminalFacility_type AS ENUM ('TerminalFacility');
+CREATE TABLE TerminalFacility (AirportFacility JSON, Identifier TEXT, Name TEXT, type TerminalFacility_type);
