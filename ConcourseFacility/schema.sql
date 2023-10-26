0fb6971092c3c3113aee8e482dcb906665ac3fa1
Alberto Abella <alberto.abella@fiware.org>
2023-10-26 15:19:58 +0200

    test of SQL export

--- ConcourseFacility/schema.sql
+++ ConcourseFacility/schema.sql
@@ -1,3 +1,3 @@
 /* (Beta) Export of data model ConcourseFacility of the subject dataModel.ACRIS for a PostgreSQL database. Pending translation of enumerations and multityped attributes */
 CREATE TYPE ConcourseFacility_type AS ENUM ('ConcourseFacility');
-CREATE TABLE ConcourseFacility (Identifier TEXT, Name TEXT, TerminalFacility JSON, type ConcourseFacility_type);
+CREATE TABLE ConcourseFacility (Identifier TEXT, Name TEXT, TerminalFacility JSON, id TEXT PRIMARY KEY, type ConcourseFacility_type);
