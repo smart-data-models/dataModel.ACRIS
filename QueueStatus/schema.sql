34d187ea800a05ac06b0d397cdccec6430510b4c
Alberto Abella <alberto.abella@fiware.org>
2023-10-26 15:20:21 +0200

    test of SQL export

--- QueueStatus/schema.sql
+++ QueueStatus/schema.sql
@@ -1,3 +1,3 @@
 /* (Beta) Export of data model QueueStatus of the subject dataModel.ACRIS for a PostgreSQL database. Pending translation of enumerations and multityped attributes */
 CREATE TYPE QueueStatus_type AS ENUM ('QueueStatus');
-CREATE TABLE QueueStatus (Name TEXT, type QueueStatus_type);
+CREATE TABLE QueueStatus (Name TEXT, id TEXT PRIMARY KEY, type QueueStatus_type);
