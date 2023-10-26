ed327953ef1323965a2536f2775c4a1b5ac11757
Alberto Abella <alberto.abella@fiware.org>
2023-10-26 15:20:01 +0200

    test of SQL export

--- MeasurementDevice/schema.sql
+++ MeasurementDevice/schema.sql
@@ -1,3 +1,3 @@
 /* (Beta) Export of data model MeasurementDevice of the subject dataModel.ACRIS for a PostgreSQL database. Pending translation of enumerations and multityped attributes */
 CREATE TYPE MeasurementDevice_type AS ENUM ('MeasurementDevice');
-CREATE TABLE MeasurementDevice (MeasurementDeviceLocation JSON, Name TEXT, type MeasurementDevice_type);
+CREATE TABLE MeasurementDevice (MeasurementDeviceLocation JSON, Name TEXT, id TEXT PRIMARY KEY, type MeasurementDevice_type);
