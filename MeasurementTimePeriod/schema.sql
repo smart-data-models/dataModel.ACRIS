bc05e49fb1b46d729fca8f2ba2c562b63b0db717
Alberto Abella <alberto.abella@fiware.org>
2023-10-26 15:20:05 +0200

    test of SQL export

--- MeasurementTimePeriod/schema.sql
+++ MeasurementTimePeriod/schema.sql
@@ -1,3 +1,3 @@
 /* (Beta) Export of data model MeasurementTimePeriod of the subject dataModel.ACRIS for a PostgreSQL database. Pending translation of enumerations and multityped attributes */
 CREATE TYPE MeasurementTimePeriod_type AS ENUM ('MeasurementTimePeriod');
-CREATE TABLE MeasurementTimePeriod (EndTime TEXT, type MeasurementTimePeriod_type);
+CREATE TABLE MeasurementTimePeriod (EndTime TEXT, id TEXT PRIMARY KEY, type MeasurementTimePeriod_type);
