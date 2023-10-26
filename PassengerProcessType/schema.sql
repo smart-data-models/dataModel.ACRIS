137a602ff95903def17479ad2f59b3ac7de1c4bd
Alberto Abella <alberto.abella@fiware.org>
2023-10-26 15:20:12 +0200

    test of SQL export

--- PassengerProcessType/schema.sql
+++ PassengerProcessType/schema.sql
@@ -1,3 +1,3 @@
 /* (Beta) Export of data model PassengerProcessType of the subject dataModel.ACRIS for a PostgreSQL database. Pending translation of enumerations and multityped attributes */
 CREATE TYPE PassengerProcessType_type AS ENUM ('PassengerProcessType');
-CREATE TABLE PassengerProcessType (Code TEXT, Description TEXT, type PassengerProcessType_type);
+CREATE TABLE PassengerProcessType (Code TEXT, Description TEXT, id TEXT PRIMARY KEY, type PassengerProcessType_type);
