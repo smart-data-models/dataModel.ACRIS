88f3dd79f7eb35cffd9c3c889ab5aa8b751428e0
Alberto Abella <alberto.abella@fiware.org>
2023-10-26 15:20:30 +0200

    test of SQL export

--- ZoneAreaLocation/schema.sql
+++ ZoneAreaLocation/schema.sql
@@ -1,3 +1,3 @@
 /* (Beta) Export of data model ZoneAreaLocation of the subject dataModel.ACRIS for a PostgreSQL database. Pending translation of enumerations and multityped attributes */
 CREATE TYPE ZoneAreaLocation_type AS ENUM ('ZoneAreaLocation');
-CREATE TABLE ZoneAreaLocation (Name TEXT, TerminalAreaLocation JSON, type ZoneAreaLocation_type);
+CREATE TABLE ZoneAreaLocation (Name TEXT, TerminalAreaLocation JSON, id TEXT PRIMARY KEY, type ZoneAreaLocation_type);
