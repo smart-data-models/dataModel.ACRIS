37221e0e12c2629679acbb042f984781430709e0
Alberto Abella <alberto.abella@fiware.org>
2023-10-26 15:20:25 +0200

    test of SQL export

--- TerminalAreaLocation/schema.sql
+++ TerminalAreaLocation/schema.sql
@@ -1,3 +1,3 @@
 /* (Beta) Export of data model TerminalAreaLocation of the subject dataModel.ACRIS for a PostgreSQL database. Pending translation of enumerations and multityped attributes */
 CREATE TYPE TerminalAreaLocation_type AS ENUM ('TerminalAreaLocation');
-CREATE TABLE TerminalAreaLocation (AirportLocation JSON, Name TEXT, type TerminalAreaLocation_type);
+CREATE TABLE TerminalAreaLocation (AirportLocation JSON, Name TEXT, id TEXT PRIMARY KEY, type TerminalAreaLocation_type);
