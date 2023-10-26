090cd674022e6dc63ca509bc8f869d6d97ee3525
Alberto Abella <alberto.abella@fiware.org>
2023-10-26 15:20:07 +0200

    test of SQL export

--- OperationTimePeriod/schema.sql
+++ OperationTimePeriod/schema.sql
@@ -1,3 +1,3 @@
 /* (Beta) Export of data model OperationTimePeriod of the subject dataModel.ACRIS for a PostgreSQL database. Pending translation of enumerations and multityped attributes */
 CREATE TYPE OperationTimePeriod_type AS ENUM ('OperationTimePeriod');
-CREATE TABLE OperationTimePeriod (ClosingTime TEXT, OpeningTime TEXT, type OperationTimePeriod_type);
+CREATE TABLE OperationTimePeriod (ClosingTime TEXT, OpeningTime TEXT, id TEXT PRIMARY KEY, type OperationTimePeriod_type);
