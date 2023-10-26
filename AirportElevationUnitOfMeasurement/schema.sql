6308c0029fbc023762f20f2b98e2b04f87073f43
Alberto Abella <alberto.abella@fiware.org>
2023-10-26 15:19:43 +0200

    test of SQL export

--- AirportElevationUnitOfMeasurement/schema.sql
+++ AirportElevationUnitOfMeasurement/schema.sql
@@ -1,3 +1,3 @@
 /* (Beta) Export of data model AirportElevationUnitOfMeasurement of the subject dataModel.ACRIS for a PostgreSQL database. Pending translation of enumerations and multityped attributes */
 CREATE TYPE AirportElevationUnitOfMeasurement_type AS ENUM ('AirportElevationUnitOfMeasurement');
-CREATE TABLE AirportElevationUnitOfMeasurement (Name TEXT, type AirportElevationUnitOfMeasurement_type);
+CREATE TABLE AirportElevationUnitOfMeasurement (Name TEXT, id TEXT PRIMARY KEY, type AirportElevationUnitOfMeasurement_type);
