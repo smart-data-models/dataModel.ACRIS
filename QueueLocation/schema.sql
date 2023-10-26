df4b17900f383f40c3822429fa52c22ee150c608
Alberto Abella <alberto.abella@fiware.org>
2023-10-26 15:20:16 +0200

    test of SQL export

--- QueueLocation/schema.sql
+++ QueueLocation/schema.sql
@@ -1,3 +1,3 @@
 /* (Beta) Export of data model QueueLocation of the subject dataModel.ACRIS for a PostgreSQL database. Pending translation of enumerations and multityped attributes */
 CREATE TYPE QueueLocation_type AS ENUM ('QueueLocation');
-CREATE TABLE QueueLocation (Name TEXT, type QueueLocation_type);
+CREATE TABLE QueueLocation (Name TEXT, id TEXT PRIMARY KEY, type QueueLocation_type);
