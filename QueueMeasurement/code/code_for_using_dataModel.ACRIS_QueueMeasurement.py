cf90f20983c9c30e4cbe432ef71298894a8711d3
Alberto Abella <alberto.abella@fiware.org>
2024-05-24 18:31:49 +0200

    created example of code

--- QueueMeasurement/code/code_for_using_dataModel.ACRIS_QueueMeasurement.py
+++ QueueMeasurement/code/code_for_using_dataModel.ACRIS_QueueMeasurement.py
@@ -24,31 +24,36 @@
 #         curl -X GET http://localhost:1026/ngsi-ld/v1/entities?local=true&limit=1000
 #         
 #         # now the python code you can use to insert some value in the context broker according to the data model
+#         # Version Warning! 
+#         # This code is designed to work with the version 0.8 of pysmartdatamodels or later
+#         # to work with earlier version you need to replace the import instruction for
+#         # from pysmartdatamodels import pysmartdatamodels as sdm
 #         
-from pysmartdatamodels import pysmartdatamodels as sdm
+#         
+import pysmartdatamodels as sdm
 import subprocess
 serverUrl = "http://localhost:1026" # supposed that your broker is installed in localhost. Edit to match your configuration
 dataModel = "QueueMeasurement"
 subject = "dataModel.ACRIS"
-Occupancy = {'type': 'Property', 'value': 58}
+Occupancy = 58
 attribute = "Occupancy"
 value = Occupancy
 # The next line creates the query for inserting this attribute in a NGSI-LD context broker if the attribute does not exist it creates it
 print(sdm.update_broker(dataModel, subject, attribute, value, serverUrl=serverUrl, updateThenCreate=True))
 
-ProjectedWaitTime = {'type': 'Property', 'value': 544.4}
+ProjectedWaitTime = 544.4
 attribute = "ProjectedWaitTime"
 value = ProjectedWaitTime
 # The next line creates the query for inserting this attribute in a NGSI-LD context broker if the attribute does not exist it creates it
 print(sdm.update_broker(dataModel, subject, attribute, value, serverUrl=serverUrl, updateThenCreate=True))
 
-Throughput = {'type': 'Property', 'value': 384}
+Throughput = 384
 attribute = "Throughput"
 value = Throughput
 # The next line creates the query for inserting this attribute in a NGSI-LD context broker if the attribute does not exist it creates it
 print(sdm.update_broker(dataModel, subject, attribute, value, serverUrl=serverUrl, updateThenCreate=True))
 
-WaitTime = {'type': 'Property', 'value': 645.9}
+WaitTime = 645.9
 attribute = "WaitTime"
 value = WaitTime
 # The next line creates the query for inserting this attribute in a NGSI-LD context broker if the attribute does not exist it creates it
