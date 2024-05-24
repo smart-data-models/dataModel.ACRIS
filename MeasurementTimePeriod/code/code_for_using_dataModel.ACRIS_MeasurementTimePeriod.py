e158a58ef9f7bdac95b9618f4cd739c6ea292d23
Alberto Abella <alberto.abella@fiware.org>
2024-05-24 18:31:27 +0200

    created example of code

--- MeasurementTimePeriod/code/code_for_using_dataModel.ACRIS_MeasurementTimePeriod.py
+++ MeasurementTimePeriod/code/code_for_using_dataModel.ACRIS_MeasurementTimePeriod.py
@@ -24,13 +24,18 @@
 #         curl -X GET http://localhost:1026/ngsi-ld/v1/entities?local=true&limit=1000
 #         
 #         # now the python code you can use to insert some value in the context broker according to the data model
+#         # Version Warning! 
+#         # This code is designed to work with the version 0.8 of pysmartdatamodels or later
+#         # to work with earlier version you need to replace the import instruction for
+#         # from pysmartdatamodels import pysmartdatamodels as sdm
 #         
-from pysmartdatamodels import pysmartdatamodels as sdm
+#         
+import pysmartdatamodels as sdm
 import subprocess
 serverUrl = "http://localhost:1026" # supposed that your broker is installed in localhost. Edit to match your configuration
 dataModel = "MeasurementTimePeriod"
 subject = "dataModel.ACRIS"
-EndTime = "{'type': 'Property', 'value': {'@type': 'date-time', '@value': '2023-03-22T18:59:02Z'}}"
+EndTime = "2023-03-22T18:59:02Z"
 attribute = "EndTime"
 value = EndTime
 # The next line creates the query for inserting this attribute in a NGSI-LD context broker if the attribute does not exist it creates it
