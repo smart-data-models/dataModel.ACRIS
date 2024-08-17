2f75a948d118094f4c6d1cd0ac114942716f7602
Alberto Abella <alberto.abella@fiware.org>
2024-08-18 01:25:37 +0200

    Draft version of pydantic file v2

---
+++ QueueType/code/code_for_using_pydantic.py
@@ -0,0 +1,30 @@
+from __future__ import annotations
+
+from enum import Enum
+from typing import Optional, Union
+
+from pydantic import AnyUrl, BaseModel, Field, constr
+
+
+class Type(Enum):
+    QueueType = 'QueueType'
+
+
+class QueueType(BaseModel):
+    Code: Optional[str] = Field(
+        None, description='Unique code for the type of Passenger Queue.'
+    )
+    Description: Optional[str] = Field(
+        None, description='Description of the type of Passenger Queue.'
+    )
+    id: Optional[
+        Union[
+            constr(
+                pattern=r'^[\\w\\-\\.\\{\\}\\$\\+\\*\\[\\]`|~^@!, :\\\\]+$',
+                min_length=1,
+                max_length=256,
+            ),
+            AnyUrl,
+        ]
+    ] = Field(None, description='Unique identifier of the entity')
+    type: Optional[Type] = Field(None, description='It must be equal to QueueType.')
