028d8e1f2ffcf294473609f5bbf3f01eb2c77688
Alberto Abella <alberto.abella@fiware.org>
2024-08-18 01:25:24 +0200

    Draft version of pydantic file v2

---
+++ QueueLocation/code/code_for_using_pydantic.py
@@ -0,0 +1,25 @@
+from __future__ import annotations
+
+from enum import Enum
+from typing import Optional, Union
+
+from pydantic import AnyUrl, BaseModel, Field, constr
+
+
+class Type(Enum):
+    QueueLocation = 'QueueLocation'
+
+
+class QueueLocation(BaseModel):
+    Name: Optional[str] = Field(None, description='Unique name for the Queue Location.')
+    id: Optional[
+        Union[
+            constr(
+                pattern=r'^[\\w\\-\\.\\{\\}\\$\\+\\*\\[\\]`|~^@!, :\\\\]+$',
+                min_length=1,
+                max_length=256,
+            ),
+            AnyUrl,
+        ]
+    ] = Field(None, description='Unique identifier of the entity')
+    type: Optional[Type] = Field(None, description='It must be equal to QueueLocation.')
