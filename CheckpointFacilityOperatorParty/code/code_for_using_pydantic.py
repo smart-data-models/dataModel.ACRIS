95d845c117b51562ff877a3135ce2e626a1be02a
Alberto Abella <alberto.abella@fiware.org>
2024-08-18 01:24:23 +0200

    Draft version of pydantic file v2

---
+++ CheckpointFacilityOperatorParty/code/code_for_using_pydantic.py
@@ -0,0 +1,30 @@
+from __future__ import annotations
+
+from enum import Enum
+from typing import Optional, Union
+
+from pydantic import AnyUrl, BaseModel, Field, constr
+
+
+class Type(Enum):
+    CheckpointFacilityOperatorParty = 'CheckpointFacilityOperatorParty'
+
+
+class CheckpointFacilityOperatorParty(BaseModel):
+    Name: Optional[str] = Field(
+        None,
+        description='Unique name of the Operator Party for the Checkpoint Facility.',
+    )
+    id: Optional[
+        Union[
+            constr(
+                pattern=r'^[\\w\\-\\.\\{\\}\\$\\+\\*\\[\\]`|~^@!, :\\\\]+$',
+                min_length=1,
+                max_length=256,
+            ),
+            AnyUrl,
+        ]
+    ] = Field(None, description='Unique identifier of the entity')
+    type: Optional[Type] = Field(
+        None, description='It must be equal to CheckpointFacilityOperatorParty.'
+    )
